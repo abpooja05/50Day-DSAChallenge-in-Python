becc6c7913292dc9e46540a4032fc9c62a5e0c0c
abpooja05 <142305184+abpooja05@users.noreply.github.com>
2025-12-02 23:46:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+
+class Solution:
+    def evenlyDivides(self, n):
+        # code here
+        n = str(N)
+        count = 0
+        for i in n:
+            if int(i) == 0:
+                continue
+            elif N % int(i) == 0:
+                count += 1
+        return count
+        
